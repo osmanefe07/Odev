b8106671db6fb3d8d93a375ff0d87177eb36d667
Osman <Osman@DESKTOP-FVSD84U>
2023-12-04 03:36:22 +0300

    Add project files.

---
+++ Controllers/AdminController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Odev.Controllers
+{
+    public class AdminController : Controller
+    {
+        [Authorize]
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ Models/AppDbContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Odev.Models;
+
+
+namespace Odev.Models
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions options) : base(options)
+        {
+        }
+
+      
+
+        public DbSet<Car>Cars { get; set; }
+
+        public DbSet<User> Users { get; set; }
+    }
+}
